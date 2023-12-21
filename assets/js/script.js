53bfb4c55eeb615f0620427d9b17b42c3b5824a9
leansdr <leansdr@gmail.com>
2023-12-21 01:00:00 -0300

    Mudando o botão toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 const toggle = () => {
   let body = document.getElementById("body");
-  btn.classList.toggle("on");
+  circle.classList.toggle("on");
   body.classList.toggle("dark");
-  btn.classList.toggle("border-button");
 };
 
-let btn = document.getElementById("button-toogle");
-btn.addEventListener("click", toggle);
+let circle = document.getElementById("circle");
+circle.addEventListener("click", toggle);
